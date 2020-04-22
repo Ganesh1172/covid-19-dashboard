6ba3263dc575ddf7ff34b6b24558a4aeed569ee1
ganesh1172 <ganesh152595@gmail.com>
2020-04-22 18:16:06 +0530

    added script for selecting countries names

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+import { fetchCountries } from "../../api";
+import styles from "./CountryPicker.module.css";
 
 const CountryPicker = () => {
-  return <h1>CountryPicker</h1>;
+  const [fetchedCountries, setfetchedCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setfetchedCountries(await fetchCountries);
+    };
+    fetchAPI();
+  }, [setfetchedCountries]);
+  console.log(fetchedCountries);
+  return (
+    <FormControl className={styles.FormControl}>
+      <NativeSelect>
+        <option value="global">Global</option>
+      </NativeSelect>
+    </FormControl>
+  );
 };
 
 export default CountryPicker;
