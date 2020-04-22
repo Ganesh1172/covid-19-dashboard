6eb36b96d9ce9720842b8b684b3bbdb2d237a959
ganesh1172 <ganesh152595@gmail.com>
2020-04-22 21:23:47 +0530

    Updates

---
+++ precache-manifest.a5a27166e45f2021b47178a9bd528c9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15b08e1e4607e11f380bd9c1f63e0d0",
+    "url": "/covid-19-dashboard/index.html"
+  },
+  {
+    "revision": "ae2d6903b47db51eeaf4",
+    "url": "/covid-19-dashboard/static/css/main.8d249fdb.chunk.css"
+  },
+  {
+    "revision": "7cd8c530dfb57a10e535",
+    "url": "/covid-19-dashboard/static/js/2.e746076f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-dashboard/static/js/2.e746076f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2d6903b47db51eeaf4",
+    "url": "/covid-19-dashboard/static/js/main.eacb1b59.chunk.js"
+  },
+  {
+    "revision": "11c505f39b48ba1c248a",
+    "url": "/covid-19-dashboard/static/js/runtime-main.25797e7a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-dashboard/static/media/covid.d7265326.png"
+  }
+]);
