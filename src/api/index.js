f1afbb4c53a8788a646bb9caaa3cdcda1fd9b0e3
ganesh1172 <ganesh152595@gmail.com>
2020-04-22 21:09:27 +0530

    added function fetch countries data from api

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,16 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+    } = await axios.get(changeableUrl);
 
     // const modifiedData = {
     //   confirmed,
@@ -37,8 +42,10 @@ export const fetchDailyData = async () => {
 
 export const fetchCountries = async () => {
   try {
-    const response = await axios.get(`${url}/countries`);
-    console.log(response);
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
   } catch (error) {
     console.log(error);
   }
